6a21899f14441a6a7f6f3aaa565ef766cee38e74
HoangPhung98 <hoangpht287@gmail.com>
2017-12-16 21:38:56 +0700

    fix OOP

--- src/Ducks.java
+++ src/Ducks.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 
 public class Ducks {
 	
@@ -9,14 +10,79 @@ public class Ducks {
 	 public  boolean isGone ;
 	 public  boolean canFlyAway ;
 	 public  int  hitWallTimes ;
-	 
-	 
-//	 public Ducks(){
-//		 xCoordinate = 0;
-//		 yCoordinate = 0;
-//		 xdirection = 0;
-//		 ydirection = 0;
-//	 }
+	 private final int FROM_LEFT_WALL = 0;
+	 private final int FROM_BOTTOM_WALL = 1;
+	 private final int FROM_RIGHT_WALL= 2;
+	 void addDuck(){
+			this.yCoordinate = getCoordinateOfDuck().y;
+			this.xCoordinate = getCoordinateOfDuck().x;
+			setRamdomDirection();
+			
+			
+	}
+	void updateDuckPosition(){
+			this.xCoordinate += this.xdirection;
+			this.yCoordinate += this.ydirection;
+			checkHitWall();
+		}
+	 private  Point getCoordinateOfDuck(){
+			
+			int y=20;
+			int x = StdRandom.uniform(20, GameFrame.getWidth()-20);
+			if(StdRandom.uniform(3)==FROM_LEFT_WALL) {
+				x=20;
+				y = StdRandom.uniform(20,GameFrame.HEIGHT-20);
+			}
+			if(StdRandom.uniform(3)==FROM_BOTTOM_WALL) {
+				y=20;
+				x = StdRandom.uniform(20, GameFrame.getWidth()-20);
+			}
+			if(StdRandom.uniform(3)==FROM_RIGHT_WALL) {
+				x=GameFrame.WIDTH-20;
+				y = StdRandom.uniform(20,GameFrame.HEIGHT-20);
+			}
+			
+		
+			return new Point(x, y);
+		
+		}
+		private  void setRamdomDirection(){
+			if(StdRandom.uniform(0,2)==0){
+				this.xdirection=-50;
+			}else this.xdirection = 50;
+			if(StdRandom.uniform(0,2)==0){
+				this.ydirection = -50;
+			}else this.ydirection = 50;
+		}
+		void setDuckDead(){
+			this.xdirection =0;
+			this.ydirection =-100;
+		}
+		private void checkHitWall(){
+			// if the duck hit the wall 4 times then it can fly away
+			if(this.yCoordinate +20> GameFrame.getHeight()){
+				if(!checkHit4Times(this)){
+					this.ydirection *=-1;
+					this.hitWallTimes++;
+				}else{
+					this.isGone = true;
+				}
+			}
+			
+			//the duck is turn back if hit the wall
+			if(this.xCoordinate +20> GameFrame.getWidth() || this.xCoordinate -20< 0) {
+				this.xdirection *=-1;
+				this.hitWallTimes ++;
+			}
+			if( this.yCoordinate-20< 0) {
+				this.ydirection *=-1;
+				this.hitWallTimes ++;
+			}
+		}
+		private boolean checkHit4Times(Ducks duck){
+			if(duck.hitWallTimes>4) return true;
+			else return false;
+		}
 	
 	
 }
--- src/InGame.java
+++ src/InGame.java
@@ -17,15 +17,13 @@ public class InGame  implements Runnable{
 	Thread thread, threadMouse;
 	static double xCross, yCross;
 	double crossDegree = 0;
-	final int valocity = 50;
-	final int duckPading = 25;
+	final int valocity = 100;
+	final int duckPading = 45;
 	private long time;
-	private final int NUMBER_OF_DUCKS = 10;
+	private final int NUMBER_OF_DUCKS = 4;
 	Ducks [] flyingDucks;
 	
-	private final int FROM_LEFT_WALL = 0;
-	private final int FROM_BOTTOM_WALL = 1;
-	private final int FROM_RIGHT_WALL= 2;
+	
 	
 	Point point;
 	public InGame() {
@@ -38,7 +36,7 @@ public class InGame  implements Runnable{
 		//vẽ tâm ban đầu
 		xCross = StdDraw.mouseX(); 
 		yCross = StdDraw.mouseY();
-		drawCross(xCross, yCross);
+		drawCross();
 		
 		createMatrix();
 		
@@ -53,10 +51,10 @@ public class InGame  implements Runnable{
 	
 	}
 	
-	public void drawCross(double x, double y){
+	public void drawCross(){
 		
 		drawBackGround();
-		StdDraw.picture(x, y, "crosshair.png",crossDegree);
+		StdDraw.picture(StdDraw.mouseX(), StdDraw.mouseY(), "crosshair.png",crossDegree);
 
 	}
 	
@@ -64,7 +62,7 @@ public class InGame  implements Runnable{
 		
 		StdDraw.setPenColor(StdDraw.BLACK);
 		for(int i =0 ; i<NUMBER_OF_DUCKS; i++){
-			if(!flyingDucks[i].isGone)StdDraw.filledCircle(flyingDucks[i].xCoordinate, flyingDucks[i].yCoordinate, 20);
+			if(!flyingDucks[i].isGone)StdDraw.filledCircle(flyingDucks[i].xCoordinate, flyingDucks[i].yCoordinate, 45);
 		}
 		StdDraw.picture(GameFrame.HALF_WIDTH, 85, "grass.png", GameFrame.WIDTH, 169.6493);
 		StdDraw.show(10);
@@ -74,34 +72,25 @@ public class InGame  implements Runnable{
 	}
 	
 		
-	private  void addDuck(int i){
-		flyingDucks[i].yCoordinate = getCoordinateOfDuck().y;
-		flyingDucks[i].xCoordinate = getCoordinateOfDuck().x;
-		setRamdomDirection(i);
-		
-		
-	}
 	
 	
 	@Override
 	public void run() {
 		time = System.currentTimeMillis();
 		for(int i=0; i<NUMBER_OF_DUCKS; i++){
-			addDuck(i);
+			flyingDucks[i].addDuck();
 	
 		}
 
 		while(true){
 			
-			drawCross(StdDraw.mouseX(), StdDraw.mouseY());
-			setNewXYCross();
-			
+			drawCross();
 			if(checkCanMove(time)){
-				for(int i=0; i<NUMBER_OF_DUCKS; i++)updateDuckPosition(i);
+				for(int i=0; i<NUMBER_OF_DUCKS; i++)flyingDucks[i].updateDuckPosition();
 				time= System.currentTimeMillis();
 			}
 			drawDuck();
-
+			checkShotDuck();
 
 
 			try {
@@ -113,89 +102,29 @@ public class InGame  implements Runnable{
 		}
 		
 	}
-	private void updateDuckPosition(int i){
-		flyingDucks[i].xCoordinate += flyingDucks[i].xdirection;
-		flyingDucks[i].yCoordinate += flyingDucks[i].ydirection;
-		checkHitWall(i);
-	}
-	private  Point getCoordinateOfDuck(){
-		
-		int y=20;
-		int x = StdRandom.uniform(20, gameFrame.getWidth()-20);
-		if(StdRandom.uniform(3)==FROM_LEFT_WALL) {
-			x=20;
-			y = StdRandom.uniform(20,gameFrame.HEIGHT-20);
-		}
-		if(StdRandom.uniform(3)==FROM_BOTTOM_WALL) {
-			y=20;
-			x = StdRandom.uniform(20, gameFrame.getWidth()-20);
-		}
-		if(StdRandom.uniform(3)==FROM_RIGHT_WALL) {
-			x=gameFrame.WIDTH-20;
-			y = StdRandom.uniform(20,gameFrame.HEIGHT-20);
-		}
-		
 	
-		return new Point(x, y);
 	
-	}
-	private  void setRamdomDirection(int i){
-		if(StdRandom.uniform(0,2)==0){
-			flyingDucks[i].xdirection=-50;
-		}else flyingDucks[i].xdirection = 50;
-		if(StdRandom.uniform(0,2)==0){
-			flyingDucks[i].ydirection = -50;
-		}else flyingDucks[i].ydirection = 50;
-	}
 	/////////////////////////////////////////////
-	private boolean checkShotDuck(){
-		for(int i=0; i< NUMBER_OF_DUCKS; i++){
-		if (StdDraw.isMousePressed() && StdDraw.mouseX() < flyingDucks[i].xCoordinate + duckPading && StdDraw.mouseX() > flyingDucks[i].xCoordinate - duckPading
-			&& StdDraw.mouseY() < flyingDucks[i].yCoordinate + duckPading && StdDraw.mouseY() > flyingDucks[i].yCoordinate - duckPading	) {
-			System.out.println("shoted");
-			return true;
-		}
-		}
-		return false;
-		
-	}
-	private void checkHitWall(int i){
-		// if the duck hit the wall 4 times then it can fly away
-		if(flyingDucks[i].yCoordinate +20> gameFrame.getHeight()){
-			if(!checkHit4Times(flyingDucks[i])){
-				flyingDucks[i].ydirection *=-1;
-				flyingDucks[i].hitWallTimes++;
-			}else{
-				flyingDucks[i].isGone = true;
+	private void checkShotDuck(){
+		if(StdDraw.isMousePressed()){
+			for(int i=0; i< NUMBER_OF_DUCKS; i++){
+				if ( StdDraw.mouseX() < flyingDucks[i].xCoordinate + duckPading && StdDraw.mouseX() > flyingDucks[i].xCoordinate - duckPading
+						&& StdDraw.mouseY() < flyingDucks[i].yCoordinate + duckPading && StdDraw.mouseY() > flyingDucks[i].yCoordinate - duckPading	) {
+					System.out.println("shoted");
+					flyingDucks[i].setDuckDead();
+				}
 			}
 		}
 		
-		//the duck is turn back if hit the wall
-		if(flyingDucks[i].xCoordinate +20> gameFrame.getWidth() || flyingDucks[i].xCoordinate -20< 0) {
-			flyingDucks[i].xdirection *=-1;
-			flyingDucks[i].hitWallTimes ++;
-		}
-		if( flyingDucks[i].yCoordinate-20< 0) {
-			flyingDucks[i].ydirection *=-1;
-			flyingDucks[i].hitWallTimes ++;
-		}
 	}
-	private boolean checkHit4Times(Ducks duck){
-		if(duck.hitWallTimes>4) return true;
-		else return false;
-	}
+	
+	
 	private boolean checkCanMove(long time){
 		if(System.currentTimeMillis() - time > valocity) return true;
 		return false;
 	}
-	private boolean compareXY(){
-		if(StdDraw.mouseX()!= xCross || StdDraw.mouseY()!= yCross ) return false;
-		return true;
-	}
-	private void setNewXYCross(){
-		this.xCross = StdDraw.mouseX();
-		this.yCross = StdDraw.mouseY();
-	}
+
+
 	private void createMatrix(){
 		flyingDucks = new Ducks[NUMBER_OF_DUCKS];
 		for(int i=0; i<NUMBER_OF_DUCKS; i++){
